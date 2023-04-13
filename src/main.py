8ce19f0cabd41097991c961b925813e7cb1e90c8
codyconfer <mail@codyconfer.me>
2023-04-13 17:44:30 -0600

    add formatter, logging, and event handlers

--- src/main.py
+++ src/main.py
@@ -1,24 +1,33 @@
+import logging
 import discord
 from discord.ext import commands
 from config import CONFIG
+from events.on_message import OnMessage
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s |  %(levelname)s - %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+log = logging.getLogger()
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    log.info(f"{bot.user} has connected to Discord!")
 
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
+        log.info("message is from bot")
         return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    handler = OnMessage(bot)
+    await handler.handle_hello(message)
 
 
 bot.run(CONFIG.TOKEN)
