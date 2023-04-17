3b7a666b8fcee6d4f74931001f20e65ffdbc5e83
codyconfer <mail@codyconfer.me>
2023-04-17 13:04:51 -0600

    blaze it

--- src/events/on_message.py
+++ src/events/on_message.py
@@ -22,7 +22,7 @@ class OnMessage(ActionHandler):
                 return "Hello!"
             case s if "hitler" in s:
                 return "HITLER!"
-            case s if " 420 " in s:
+            case s if "420" in s:
                 return "Blaze it!"
 
     async def action(self):
