a6f7236b46256cf935435ed1a6a0fa9c2c9f350d
codyconfer <mail@codyconfer.me>
2023-04-20 15:19:56 -0600

    allow words between 4 and 20

--- src/events/on_message.py
+++ src/events/on_message.py
@@ -24,6 +24,8 @@ class OnMessage(ActionHandler):
                 return "HITLER!"
             case s if "420" in ''.join(filter(str.isalnum, s)):
                 return "Blaze it!"
+            case s if "4" in s and "20" in s:
+                return "Blaze it!"
 
     async def action(self):
         if self.msg.author == self.bot.user:
